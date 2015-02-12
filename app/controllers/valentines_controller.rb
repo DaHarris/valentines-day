2c0f990f16175a4fba7754d44b03a2a25b4d1199
Daniel Harris <bobth3bum@gmail.com>
2015-02-12 14:51:48 -0700

    added new valentines view

--- app/controllers/valentines_controller.rb
+++ app/controllers/valentines_controller.rb
@@ -1,3 +1,16 @@
 class ValentinesController < ApplicationController
 
+  def new
+    @valentine = Valentine.new
+  end
+
+  def create
+    @valentine = Valentine.new(valentine_params)
+    if @valentine.save
+      redirect_to root_path, :notice => 'Thank you!!!!!!'
+    else
+      redirect_to new_valentine_path, :notice => ':((((( try again plz.'
+    end
+  end
+
 end
